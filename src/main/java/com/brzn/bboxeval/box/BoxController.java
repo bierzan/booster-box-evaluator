5de45c1517f5287bbf47777e89688d211c15acdb
bierzan@gmail.com <bierzan@gmail.com>
2019-10-05 05:44:35 +0200

    controller for booster box data

---
+++ src/main/java/com/brzn/bboxeval/box/BoxController.java
@@ -0,0 +1,9 @@
+package com.brzn.bboxeval.box;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("box/")
+public class BoxController {
+}
