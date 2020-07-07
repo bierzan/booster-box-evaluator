7d69085298eaf7abbadcbf996c880de0f348a02e
bierzan@gmail.com <bierzan@gmail.com>
2020-07-07 22:52:54 +0200

    MtgIOClient - tests + refactor

--- src/test/groovy/com/brzn/box_eval/mtg_io_client/MtgIOClientTest.groovy
+++ src/test/groovy/com/brzn/box_eval/mtg_io_client/MtgIOClientTest.groovy
@@ -1,9 +1,10 @@
 package com.brzn.box_eval.mtg_io_client
 
 import com.brzn.box_eval.mtgIOclient.domain.SampleCardSets
+import com.brzn.box_eval.mtg_io_client.dto.CardSet
 import com.brzn.box_eval.mtg_io_client.dto.CardSetsArray
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.common.collect.Lists
+import io.vavr.collection.HashSet
 import io.vavr.collection.List
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -13,11 +14,15 @@ import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
+import java.nio.charset.StandardCharsets
+import java.util.stream.Collectors
+
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
 
 class MtgIOClientTest extends Specification implements SampleCardSets {
+    public static final String GET_SETS_BY_NAMES_URL = "https://api.magicthegathering.io/v1/sets?name="
     private RestTemplate restTemplate = new RestTemplate()
     private MockRestServiceServer mockServer;
     MtgIOClient client = new MtgIOClient(restTemplate);
@@ -27,23 +32,57 @@ class MtgIOClientTest extends Specification implements SampleCardSets {
         mockServer = MockRestServiceServer.createServer(restTemplate);
     }
 
-    def "should return and deserialize sampleSet"() { //todo do refactoru
+    def "should return and deserialize sampleSet"() {
         given:
-        def names = new SearchedCardSets(List.of("khans2").toSet())
-        mockServer.expect(requestTo("https://api.magicthegathering.io/v1/sets?name=khans"))
+        def searchedCardSets = new SearchedCardSets(HashSet.of(sampleCommonSet.name))
+        def JSONCardSetArray = getJSONCardSetArrayFromCardSets(sampleCommonSet)
+        def expectedCardSets = getCardSetsFromJSONCardSetsArray(JSONCardSetArray);
+
+        mockServer.expect(requestTo(GET_SETS_BY_NAMES_URL + encodeURLQuery(sampleCommonSet.name)))
                 .andExpect(method(HttpMethod.GET))
                 .andRespond(withStatus(HttpStatus.OK)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .body(json2));
-        def expected = objectMapper.readValue(json2, CardSetsArray.class);
+                        .body(JSONCardSetArray));
         when:
-        def wynik = client.findCardSetsByName(names)
+        def result = client.findCardSetsByName(searchedCardSets)
         then:
         mockServer.verify()
-        wynik.toJavaList() == expected.getSets()
+        result == expectedCardSets
     }
 
-    def json2 = objectMapper.writeValueAsString(new CardSetsArray(Lists.newArrayList(sampleCommonSet)))
+    def "should return and deserialize array of sampleCommonSet and sampleMasterSet"() {
+        given:
+        def searchedCardSets = new SearchedCardSets(HashSet.of(sampleCommonSet.name, sampleMastersSet.name))
+        def JSONCardSetArray = getJSONCardSetArrayFromCardSets(sampleCommonSet, sampleMastersSet)
+        def expectedCardSets = getCardSetsFromJSONCardSetsArray(JSONCardSetArray);
+
+        mockServer.expect(requestTo(GET_SETS_BY_NAMES_URL + encodeURLQuery(sampleCommonSet.name, sampleMastersSet.name)))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(JSONCardSetArray));
+        when:
+        def result = client.findCardSetsByName(searchedCardSets)
+        then:
+        mockServer.verify()
+        result == expectedCardSets
+    }
+
+    private String encodeURLQuery(String... setNames) {
+        def setNamesAsSingleString = Arrays.asList(setNames)
+                .reverse()
+                .stream()
+                .collect(Collectors.joining("|"))
+        return URLEncoder.encode(setNamesAsSingleString, StandardCharsets.UTF_8.toString())
+    }
+
+    private List<CardSet> getCardSetsFromJSONCardSetsArray(String jsonArray) {
+        return List.ofAll(objectMapper.readValue(jsonArray, CardSetsArray.class).getSets())
+    }
+
+    private String getJSONCardSetArrayFromCardSets(CardSet... sets) {
+        return objectMapper.writeValueAsString(new CardSetsArray(Arrays.asList(sets)))
+    }
 
     def json = '{\n' +
             '   "sets":[\n' +
